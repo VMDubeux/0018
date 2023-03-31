93dd5751dbff78ba575b7376d46ac5543f42b793
Victor Moura Dubeux <adv.victordubeux@gmail.com>
2023-03-31 00:10:15 -0300

    Commit 2
    Criação de objetos, materiais, scripts, codificação de atributos e métodos, uso do SerializeField, Input.GetAxis, Time.deltaTime, GetComponent<>, entre outras coisas.

---
+++ 0018/Assets/Mover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Move with WASD or arrows keys.");
+        Debug.Log("Dodge all the obstacles and reach the peaceful zone.");
+    }
+
+    void MovePlayer() 
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
+    }
+}
